3c9e051e83265ea0f7262214c09a3cde134f6f38
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-05-14 04:20:49 -0400

    new system for checking winning moves

--- player.rb
+++ player.rb
@@ -9,7 +9,6 @@ class Player
   end
 
   def has_won?
-    # do any 3 of @my_moves sum to 12?
     @my_moves.combination(3).detect { |a, b, c| a + b + c == 12 }
   end
 
@@ -18,8 +17,8 @@ class Player
   end
 
   def winning_moves
-    moves = @pairs.map.with_index { |p,i| 12 - i if p }
-    moves.select { |m| open_squares.include?(m) }
+    moves = @my_moves.combination(2).detect { |a, b| open_squares.include?( 12 - (a + b )) }
+    12 - moves.inject(:+) if moves
   end
 
   private def open_squares
@@ -30,6 +29,7 @@ end
 
 class HumanPlayer < Player
   def take_turn(opponent)
+    puts winning_moves
     mark = gets.chomp.to_i
     if open_squares.include?(mark)
       record(mark)
@@ -48,7 +48,13 @@ class ComputerPlayer < Player
   # take whatever is free
 
   def take_turn(opponent)
+    if winning_moves
+      mark = winning_moves
+    elsif opponent.winning_moves
+      mark = opponent.winning_moves
+    else
       mark = rand(0..9)
+    end
     if open_squares.include?(mark)
       record(mark)
       mark
